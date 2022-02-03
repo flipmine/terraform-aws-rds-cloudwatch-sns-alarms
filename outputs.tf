1c33cf7713e6657f0bfac8773e370d72736e043e
David Gillis <david@flipmine.com>
2022-02-03 12:22:34 -0500

    add more outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,38 @@ output "sns_topic_arn" {
   description = "The ARN of the SNS topic"
   value       = join("", aws_sns_topic.default.*.arn)
 }
+
+output "burst_balance_too_low_name" {
+  description = "Average database storage burst balance over last 10 minutes too low, expect a significant performance drop soon"
+  value       = aws_cloudwatch_metric_alarm.burst_balance_too_low.alarm_name
+}
+
+output "cpu_utilization_too_high_name" {
+  description = "Average database CPU utilization over last 10 minutes too high"
+  value       = aws_cloudwatch_metric_alarm.cpu_utilization_too_high.alarm_name
+}
+
+output "cpu_credit_balance_too_low_name" {
+  description = "Average database CPU credit balance over last 10 minutes too low, expect a significant performance drop soon"
+  value       = aws_cloudwatch_metric_alarm.cpu_credit_balance_too_low.alarm_name
+}
+
+output "disk_queue_depth_too_high_name" {
+  description = "Average database disk queue depth over last 10 minutes too high, performance may suffer"
+  value       = aws_cloudwatch_metric_alarm.disk_queue_depth_too_high.alarm_name
+}
+
+output "freeable_memory_too_low_name" {
+  description = "Average database freeable memory over last 10 minutes too low, performance may suffer"
+  value       = aws_cloudwatch_metric_alarm.freeable_memory_too_low.alarm_name
+}
+
+output "free_storage_space_too_low_name" {
+  description = "Average database free storage space over last 10 minutes too low"
+  value       = aws_cloudwatch_metric_alarm.free_storage_space_too_low.alarm_name
+}
+
+output "swap_usage_too_high_name" {
+  description = "Average database swap usage over last 10 minutes too high, performance may suffer"
+  value       = aws_cloudwatch_metric_alarm.swap_usage_too_high.alarm_name
+}
